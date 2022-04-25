7d0b6b3c90c2cae39f60ed4e158c57bb3ff94bbf
Dmitriy <dmitriy.tackle@gmail.com>
2022-04-26 01:24:17 +0300

    refactoring code in Rating

--- src/common/rating/Rating.jsx
+++ src/common/rating/Rating.jsx
@@ -1,27 +1,25 @@
 import React, { useState, useEffect } from "react";
 import "./rating.scss";
 
-const Rating = (props) => {
+const Rating = ({ vote }) => {
   const [style, setStyle] = useState({});
-
-  console.log(props.vote);
 
   useEffect(() => {
     const newStyle = {
       opacity: 1,
-      width: `${props.vote * 10}%`,
+      width: `${vote * 10}%`,
     };
     setTimeout(() => {
       setStyle(newStyle);
     }, 400);
-  }, [props]);
+  }, []);
 
   return (
     <>
       <h2>Rating:</h2>
       <div className="rating">
         <div className="rating__value">
-          <p>{props.vote}</p>
+          <p>{vote}</p>
         </div>
         <div className="rating__line" style={style}></div>
       </div>
